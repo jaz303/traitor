3f22476e267c820e7e086b51ddf2f01034353d87
Jason Frame <jason@onehackoranother.com>
2014-04-05 17:43:04 +0100

    define _traits based on final list of named traits, rather than initial list
    this means the _traits list doesn't include anonymous traits, but this is no great loss

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ exports.extend      = extend;
 
 function make(traits, opts) {
 
-    traits = registry.expand(traits);
-
-    var builder = new TraitBuilder(traits);
+    var builder = new TraitBuilder(registry.expand(traits));
 
     var ctor = builder.__compile__(opts || {});
 
     // FIXME: this is a total hack and should be destroyed
     // (only exists because some tests still rely on it)
-    Object.defineProperty(ctor.prototype, '_traits', {
-        get: function() { return traits.slice(0); }
-    });
+    var namedTraits = Object.keys(builder._namedTraits);
+    if (Object.freeze) {
+        Object.freeze(namedTraits);
+        ctor.prototype._traits = namedTraits;
+    } else {
+        Object.defineProperty(ctor.prototype, '_traits', {
+            get: function() { return namedTraits.slice(0); }
+        });
+    }
 
     return ctor;
 
